e5d3fb8544278da18a040f11b958de224ca7af14
Yakub Hariana <yakubhariana70@gmail.com>
2023-09-15 00:02:54 +0700

    add CORS origin and methods setting

--- server/index.js
+++ server/index.js
@@ -10,10 +10,7 @@ const morgan = require("morgan");
 const app = express();
 
   // Middleware
-  app.use(cors({
-    origin: "*",
-    methods:["GET"],
-  }));
+  app.use(cors());
   app.use(express.json());
   app.use(morgan("dev"));
 
