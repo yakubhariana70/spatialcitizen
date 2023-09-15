e9a97aa79a726f006828e6c63a7eda0e626ae1d5
Yakub Hariana <yakubhariana70@gmail.com>
2023-09-15 16:51:52 +0700

    change POSTGRES URL

--- client/src/api/FetchData.js
+++ client/src/api/FetchData.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "https://spatialcitizen-server.vercel.app/",
+    baseURL: "http://localhost:4000",
 })
--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,8 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-})
+const pool = new Pool(
+    {connectionString: process.env.POSTGRES_URL + "?sslmode=require",}
+    )
 module.exports = {
     query: (text, params) => pool.query(text, params),
 }
