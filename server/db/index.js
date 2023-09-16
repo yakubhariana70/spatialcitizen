befaf2aea178ac6200e7aafe13a084fe96bd1dc6
Yakub Hariana <yakubhariana70@gmail.com>
2023-09-16 21:36:04 +0700

    setting database supabase

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool(
-    {connectionString: process.env.POSTGRES_URL + "?sslmode=require",}
+    {connectionString: process.env.POSTGRES_URL}
     )
 module.exports = {
     query: (text, params) => pool.query(text, params),
