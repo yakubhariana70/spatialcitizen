501ea4b45714fa5010fa99556091342accac3268
Yakub Hariana <yakubhariana70@gmail.com>
2023-10-03 20:30:46 +0700

    Setting Server ENV

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,17 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 require("dotenv").config({
-    path: "../../.env",
-  });
+  path: "../../.env",
+});
 
-const pool = new Pool(
-    {connectionString: process.env.POSTGRES_URL}
-    )
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+const proConfig = `${process.env.POSTGRES_URL}`;
+
+const pool = new Pool({
+  connectionString:
+    process.env.NODE_ENV === "production" ? proConfig : devConfig,
+});
+
 module.exports = {
-    query: (text, params) => pool.query(text, params),
-}
+  query: (text, params) => pool.query(text, params),
+};
